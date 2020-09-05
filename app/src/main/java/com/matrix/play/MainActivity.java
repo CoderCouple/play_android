329cc3139a3fdcff66ae84a7d8eff90879eac90d
CoderCouple <sunil28071987@gmail.com>
2020-09-05 14:55:36 -0700

    Added color to FAB

--- app/src/main/java/com/matrix/play/MainActivity.java
+++ app/src/main/java/com/matrix/play/MainActivity.java
@@ -1,21 +1,25 @@
 package com.matrix.play;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.graphics.drawable.DrawableCompat;
-
 import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     public BottomNavigationViewEx bottomNav;
-    FloatingActionButton fab;
+    public ArrayList<String> colors = new ArrayList<>();
+    public FloatingActionButton fab;
+    public static int nmr = 1;
+    public static final int dnr = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +29,30 @@ public class MainActivity extends AppCompatActivity {
         fab = (FloatingActionButton) findViewById(R.id.fabPlay);
         initBottomNavigationView();
 
+        colors.add("#3cba54");
+        colors.add("#f4c20d");
+        colors.add("#db3236");
+        colors.add("#4885ed");
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                        .parseColor(colors.get(nmr%dnr))));
+                nmr++;
+            }
+        });
     }
 
     private void initBottomNavigationView() {
         bottomNav.enableAnimation(false);
         bottomNav.enableItemShiftingMode(false);
         bottomNav.enableShiftingMode(false);
-        fab.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ColorStateList csl = AppCompatResources.getColorStateList(getApplicationContext(), R.color.colorPrimary);
-               fab.setBackgroundTintList(csl);
-            }
-        });
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                .parseColor("#BF0C34")));
     }
+
+
+
+
 }
