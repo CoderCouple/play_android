7ba80a6288849d39dca0ad192685d49293f29eb2
CoderCouple <sunil28071987@gmail.com>
2020-09-06 12:56:14 -0700

    fixed a small bug

--- app/src/main/java/com/matrix/play/MainActivity.java
+++ app/src/main/java/com/matrix/play/MainActivity.java
@@ -3,23 +3,24 @@ package com.matrix.play;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-import java.util.ArrayList;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import static com.matrix.play.Constants.colors;
+import static com.matrix.play.Constants.dnr;
+import static com.matrix.play.Constants.nmr;
+import static com.matrix.play.Constants.primaryColor;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationViewEx.OnNavigationItemSelectedListener {
 
     public BottomNavigationViewEx bottomNav;
-    public ArrayList<String> colors = new ArrayList<>();
     public FloatingActionButton fab;
-    public static int nmr = 1;
-    public static final int dnr = 4;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +29,10 @@ public class MainActivity extends AppCompatActivity {
         bottomNav = findViewById(R.id.bottom_nav_view);
         fab = (FloatingActionButton) findViewById(R.id.fabPlay);
         initBottomNavigationView();
-
-        colors.add("#3cba54");
-        colors.add("#f4c20d");
-        colors.add("#db3236");
-        colors.add("#4885ed");
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                bottomNav.setCurrentItem(2);
                 fab.setBackgroundTintList(ColorStateList.valueOf(Color
                         .parseColor(colors.get(nmr%dnr))));
                 nmr++;
@@ -48,11 +44,31 @@ public class MainActivity extends AppCompatActivity {
         bottomNav.enableAnimation(false);
         bottomNav.enableItemShiftingMode(false);
         bottomNav.enableShiftingMode(false);
+        bottomNav.setIconSize(30, 30);
+        bottomNav.setTextSize(10);
         fab.setBackgroundTintList(ColorStateList.valueOf(Color
-                .parseColor("#BF0C34")));
+                .parseColor(primaryColor)));
     }
 
-
-
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+       switch (item.getItemId()){
+           case R.id.home :
+               item.setChecked(true);
+               break;
+           case R.id.search :
+               item.setChecked(true);
+               break;
+           case R.id.play :
+               item.setChecked(true);
+               break;
+           case R.id.create :
+               item.setChecked(true);
+               break;
+           case R.id.account :
+               item.setChecked(true);
+               break;
+       }
+       return false;
+    }
 }
